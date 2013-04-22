8727804014aaa64a37532d747475ccada5f0bd6d
John F. Kanthak <johnkanthak@yahoo.com>
2013-04-22 07:45:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PestControl::Application.config.secret_token = 'cb1f11129456959500b57109373bc48a1e3f0d1574ff9ee8653637c2df3327eb14dcdcc9b1d5408d640ef4b6a6e314e75daec5e6705473afecfdda3d9ca0704f'
